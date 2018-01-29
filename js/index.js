2cc81756961ba37c16239ea0f9805b444315b748
pirlouit669 <pirlouit669@gmail.com>
2018-01-29 12:56:03 +0100

    migrazion

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -86,3 +68,79 @@ var app = {
        });
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == "")    {
+        navigator.notification.alert("Please enter username", null, "Username Missing", "OK");
+        return;
+    }
+
+    if(password == "")    {
+        navigator.notification.alert("Please enter password", null, "Password Missing", "OK");  
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://www.facile2soutenir.fr/wp-admin/admin-ajax.php?action=app_login&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password));
+    xhr.onload = function(){
+        if(xhr.responseText == "FALSE")        {
+            navigator.notification.alert("Wrong Username and Password", null, "Wrong Creds", "Try Again");
+        }
+        else if(xhr.responseText == "TRUE")        {
+            fetch_and_display_posts();
+            $("#page_two_link").click();
+        }
+    }   
+    xhr.send();
+}
+
+
+
+function open_browser(link){
+    window.open(link, '_blank', 'location=yes');
+}
+
+
+
+function fetch_and_display_posts(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://www.facile2soutenir.fr/wp-admin/admin-ajax.php?action=app_posts");
+    xhr.onload = function(){
+        var posts_array = JSON.parse(xhr.responseText);
+
+        var html = "";
+
+        for(var count = 0; count < posts_array.length; count++)        {
+            var title = posts_array[count][0];
+            var link = posts_array[count][1];
+            var date = posts_array[count][2];
+            var image = posts_array[count][3];
+
+            html = html + "<li>" + "<a href='javascript:open_browser(\"" + link + "\")'>" + "<img height='128' width='128' src='" + image + "'>" + "<h2>" + title + "</h2>" + "<p>" + date + "</p></a></li>";
+        }
+
+        document.getElementById("posts").innerHTML = html;
+        $("#posts").listview("refresh");
+    }
+    xhr.send();
+}
